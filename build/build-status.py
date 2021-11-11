2dddb977d86057e2f0e8cc1a740fe4c89b19a6e5
omeryildiz <nomeryildiz@gmail.com>
2021-11-11 21:10:13 +0300

    added blackhole node to the example of aodv routing code

--- build/build-status.py
+++ build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.35-scratch-simulator-debug', 'build/scratch/ns3.35-blackhole-debug', 'build/scratch/ns3.35-first-debug', 'build/scratch/ns3.35-myfirst-debug', 'build/scratch/subdir/ns3.35-subdir-debug']
+ns3_runnable_programs = ['build/scratch/ns3.35-aodv-routing-with-mal-debug', 'build/scratch/ns3.35-scratch-simulator-debug', 'build/scratch/ns3.35-blackhole-debug', 'build/scratch/ns3.35-manet-routing-compare-debug', 'build/scratch/ns3.35-first-debug', 'build/scratch/ns3.35-myfirst-debug', 'build/scratch/subdir/ns3.35-subdir-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
